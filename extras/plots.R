9abc21a52ebd8dd8f5163aa0174b4f83c9dacb09
Tom Harrop <twharrop@gmail.com>
2026-01-06 16:32:20 +1100

    plot

--- extras/plots.R
+++ extras/plots.R
@@ -16,6 +16,21 @@ latest_stats <- names(
 
 dt <- fread(latest_stats)
 
+# General settings
+tool_order <- c(
+  "braker.gff3" = "Braker3 (GFF)",
+  "braker.gtf" = "Braker3 (GTF)",
+  "funannotate.gff3" = "Funannotate",
+  "tiberius.gtf" = "Tiberius"
+)
+
+dt[, result_label := factor(
+  plyr::revalue(result_file, tool_order),
+  levels = tool_order
+)]
+
+# Tool specific settings
+
 busco_metrics <- c(
   `Single copy` = "results.Single copy percentage",
   `Multi copy` = "results.Multi copy percentage",
@@ -33,7 +48,12 @@ busco_data[, label := sub(" percentage", "", sub("results.", "", variable))]
 busco_data[, label := factor(label, levels = rev(names(busco_metrics)))]
 
 
-ggplot(busco_data, aes(x = result_file, y = value, fill = label)) +
-  facet_grid(~genome) +
+ggplot(busco_data, aes(x = result_label, y = value, fill = label)) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
   scale_fill_viridis_d(guide = guide_legend(title = NULL, reverse = TRUE)) +
+  scale_y_continuous(expand = 0.025) +
+  ylab(NULL) +
+  xlab(NULL) +
+  facet_grid(~genome) +
   geom_col(position = "stack")
